d339ef73a3c4aaf7d430513ab7e8a8bb46ee3dc9
Ayush Kumar <78543116+ayushspn123@users.noreply.github.com>
2023-07-19 23:39:26 +0530

    Update computed_property.js

--- Object/computed_property.js
+++ Object/computed_property.js
@@ -1,9 +1,9 @@
 const key1=  "objkey1";
 const key2=   "objekey2";
-const value1=1;
-const value2=2;
+const value1=1
+const value2=2
 const obj={
-    [key1]:value1,
+      [key1]:value1,
     [key2]:value2
 
 
